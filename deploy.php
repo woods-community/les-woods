9a6721e90732ccc5cbf98e03bf66c4095b9e8614
Rastislav Chynoransky <rastislav.chynoransky@gmail.com>
2023-01-22 15:25:45 +0100

    add deployer

---
+++ deploy.php
@@ -0,0 +1,23 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'https://github.com/woods-community/les-woods.git');
+set('http_user', '`whoami`');
+set('writable_mode', 'chown');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('les-woods.net')
+    ->set('deploy_path', '~/les-woods.net/les-woods');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
